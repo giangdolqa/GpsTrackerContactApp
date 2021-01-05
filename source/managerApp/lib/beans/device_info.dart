39b741ea643433cd328f39ac35868a73a21ec938
WangLX <wanglongxiang@dlcnkj.com>
2021-01-06 00:32:48 +0800

    Gpsトラッカー設定画面（途中

---
+++ source/managerApp/lib/beans/device_info.dart
@@ -0,0 +1,12 @@
+// 通常情報
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:toast/toast.dart';
+
+class DeviceInfo {
+  String name; // 名称
+  String HID; // ハードウェアID
+
+}
--- source/managerApp/lib/views/gpstracker_setting_view.dart
+++ source/managerApp/lib/views/gpstracker_setting_view.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:gps_tracker/beans/device_info.dart';
 
 import 'package:intl/intl.dart';
 
@@ -14,6 +15,69 @@ class GpsTrackerSettingView extends StatefulWidget {
 
 class GpsTrackerSettingViewState extends State<GpsTrackerSettingView> {
   String title = 'GPSトラッカー設定';
+  bool _blueToothFlag = false;
+  List<DeviceInfo> myDevices = [];
+  List<DeviceInfo> otherDevices = [];
+  List<Widget> myDevlist = [];
+  List<Widget> otherDevList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getMyDevListRow();
+    getOtherDevListRow();
+  }
+
+  // デバイスリストアイテム作成
+  void getMyDevListRow() async {
+    myDevlist.clear();
+    List<Widget> tmpDevlist = [];
+    // myDevices = .. TODO:  ペアリング済み設備取得
+    // DeviceInfo di = new DeviceInfo();
+    // di.name = "aaa";
+    // di.HID = "bbb";
+    // myDevices.add(di);
+    myDevices.forEach((device) {
+      tmpDevlist.add(
+        Container(
+          padding: EdgeInsets.all(5),
+          child: Row(
+            children: [
+              Text(device.name, style: TextStyle(fontSize: 14)),
+            ],
+          ),
+        ),
+      );
+    });
+    setState(() {
+      myDevlist = tmpDevlist;
+    });
+  }
+
+  void getOtherDevListRow() async {
+    otherDevList.clear();
+    List<Widget> tmpDevlist = [];
+    // myDevices = .. TODO:  ペアリング済み設備取得
+    DeviceInfo di = new DeviceInfo();
+    di.name = "aaa";
+    di.HID = "bbb";
+    otherDevices.add(di);
+    otherDevices.forEach((device) {
+      tmpDevlist.add(
+        Container(
+          padding: EdgeInsets.all(5),
+          child: Row(
+            children: [
+              Text(device.name, style: TextStyle(fontSize: 14)),
+            ],
+          ),
+        ),
+      );
+    });
+    setState(() {
+      myDevlist = tmpDevlist;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +89,78 @@ class GpsTrackerSettingViewState extends State<GpsTrackerSettingView> {
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
         children: <Widget>[
-
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Text(
+              "GPSトラッカーを設定するときはBluetoothをONにしてください。",
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Row(
+              children: [
+                Text(
+                  "Bluetooth",
+                  style: TextStyle(fontSize: 14),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Switch(
+                    value: _blueToothFlag,
+                    activeColor: Colors.green,
+                    onChanged: (value) {
+                      setState(() {
+                        _blueToothFlag = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            // padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Text(
+              "自分のデバイス",
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          Container(
+            // padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Divider(
+              color: Colors.blue.withOpacity(0.3),
+              thickness: 2,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 20),
+            child: ListView(
+              shrinkWrap: true,
+              children: myDevlist,
+            ),
+          ),
+          Container(
+            // padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Text(
+              "その他のデバイス",
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          Container(
+            // padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Divider(
+              color: Colors.blue.withOpacity(0.3),
+              thickness: 2,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 20),
+            child: ListView(
+              shrinkWrap: true,
+              children: otherDevList,
+            ),
+          ),
         ],
       ),
     );
